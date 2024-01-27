6fbda5408db2b90e57cee795502d1ed9b2a42ff3
Naguissa <naguissa@foroelectro.net>
2024-01-27 18:40:59 +0100

    Update to v.6.8.0

--- docs/search/all_4.js
+++ docs/search/all_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['hour_0',['hour',['../classuRTCLib.html#ab4131c1930333f1b75aa8bba7fb8ef41',1,'uRTCLib']]]
+  ['hour_0',['hour',['../classuRTCLib.html#ab4131c1930333f1b75aa8bba7fb8ef41',1,'uRTCLib']]],
+  ['hourmodeandampm_1',['hourModeAndAmPm',['../classuRTCLib.html#aeddba7b71c3089ede5d113bdb0011b7d',1,'uRTCLib']]]
 ];
--- docs/search/functions_8.js
+++ docs/search/functions_8.js
@@ -2,9 +2,10 @@ var searchData=
 [
   ['second_0',['second',['../classuRTCLib.html#a08f27cc309e8ac351f147a43a5b914e7',1,'uRTCLib']]],
   ['set_1',['set',['../classuRTCLib.html#a85fe897310af5e33f986a54f2e3e32a5',1,'uRTCLib']]],
-  ['set_5fmodel_2',['set_model',['../classuRTCLib.html#a0f5febc963ca4ffd6f5c72560a83d2c2',1,'uRTCLib']]],
-  ['set_5frtc_5faddress_3',['set_rtc_address',['../classuRTCLib.html#a0bc90d755bf101de52a0b904c76c4c95',1,'uRTCLib']]],
-  ['sqwgmode_4',['sqwgMode',['../classuRTCLib.html#aac4f19eca28bb842bcde486e237bbdbf',1,'uRTCLib']]],
-  ['sqwgsetmode_5',['sqwgSetMode',['../classuRTCLib.html#aea2bfdbdbe0e33f5195d80e4a0278647',1,'uRTCLib']]],
-  ['status32kout_6',['status32KOut',['../classuRTCLib.html#a126eeae8db5f9b5cf74e021149c1c9b7',1,'uRTCLib']]]
+  ['set_5f12hour_5fmode_2',['set_12hour_mode',['../classuRTCLib.html#a10400c5bfb777975f1d6ab64545f56da',1,'uRTCLib']]],
+  ['set_5fmodel_3',['set_model',['../classuRTCLib.html#a0f5febc963ca4ffd6f5c72560a83d2c2',1,'uRTCLib']]],
+  ['set_5frtc_5faddress_4',['set_rtc_address',['../classuRTCLib.html#a0bc90d755bf101de52a0b904c76c4c95',1,'uRTCLib']]],
+  ['sqwgmode_5',['sqwgMode',['../classuRTCLib.html#aac4f19eca28bb842bcde486e237bbdbf',1,'uRTCLib']]],
+  ['sqwgsetmode_6',['sqwgSetMode',['../classuRTCLib.html#aea2bfdbdbe0e33f5195d80e4a0278647',1,'uRTCLib']]],
+  ['status32kout_7',['status32KOut',['../classuRTCLib.html#a126eeae8db5f9b5cf74e021149c1c9b7',1,'uRTCLib']]]
 ];
