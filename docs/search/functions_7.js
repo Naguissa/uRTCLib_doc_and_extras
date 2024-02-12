2f2ab27c911d385965246e1396890e509abaad98
Naguissa <naguissa@foroelectro.net>
2024-02-12 10:50:25 +0100

    Update docs to release 6.9.0

--- docs/search/functions_7.js
+++ docs/search/functions_7.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['ramread_0',['ramRead',['../classuRTCLib.html#af8eea2501d6b82726ce6226529c82a89',1,'uRTCLib']]],
   ['ramwrite_1',['ramWrite',['../classuRTCLib.html#a6f45e764cf230086760482d671ddf142',1,'uRTCLib']]],
-  ['refresh_2',['refresh',['../classuRTCLib.html#a8f4aee6683c23572e719a94479e13541',1,'uRTCLib']]]
+  ['refresh_2',['refresh',['../classuRTCLib.html#afac6f8fa6de44e47471ab38848adf793',1,'uRTCLib']]]
 ];
